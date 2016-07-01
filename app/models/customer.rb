2121bda1fa4ddeb4c3d13ec003aba94e370ed75d
Mayur <mayursingh1220@gmail.com>
2016-07-01 18:52:48 +0530

    minor

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,14 +5,5 @@ class Customer < User
   validates :name, presence: true
   validates :email, presence: true
   validates :password, presence: true
-  # validates_confirmation_of :password
-
-  # validate def password_and_confirm_password
-  #   if self.password.present? and self.password_confirmation.present? and self.password == self.confirm_password
-  #     true
-  #   else
-  #     self.errors.add(:password, 'password not matched')
-  #   end
-  # end
 
 end
