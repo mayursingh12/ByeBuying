7ca2c8887ac591f682e72563925831936d389b73
Mayur <mayursingh1220@gmail.com>
2016-07-09 14:37:11 +0530

    case sensitive validation in subcategory

--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -2,7 +2,7 @@ class Subcategory < ActiveRecord::Base
 
   belongs_to :category, class_name: 'Category', foreign_key: :category_id
 
-  validates :name, presence: true, :uniqueness => {:scope => [:name, :category_id], :message => "already exists"}
   validates :category_id, presence: true
+  validates :name, presence: true, :uniqueness => {:scope => [:name, :category_id], :message => "already exists", case_sensitive: false }
 
 end
