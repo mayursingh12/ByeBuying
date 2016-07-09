cf33bf58d09bb007ddd75b3bb6970e4a32b7c984
Mayur <mayursingh1220@gmail.com>
2016-07-09 21:50:26 +0530

    unique category name

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ActiveRecord::Base
 
   belongs_to :subcategory
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: {case_sensitive: false}
 
 end
--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -3,6 +3,6 @@ class Subcategory < ActiveRecord::Base
   belongs_to :category, class_name: 'Category', foreign_key: :category_id
 
   validates :category_id, presence: true
-  validates :name, presence: true, :uniqueness => {:scope => [:name, :category_id], :message => "already exists", case_sensitive: false }
+  validates :name, presence: true, uniqueness: {:scope => [:name, :category_id], :message => "already exists", case_sensitive: false }
 
 end
