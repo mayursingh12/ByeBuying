75072c80541f432fb34ec1d274354b852e193871
Mayur <mayursingh1220@gmail.com>
2016-07-11 15:32:46 +0530

    jscript loading bug

--- app/assets/javascripts/customer.js
+++ app/assets/javascripts/customer.js
@@ -1,15 +1,14 @@
-/*
- *= require ./vendors/moment.min
- *= require_tree .
- *= require_self
- *= require ./vendors/bootstrap
- *= require ./vendors/loading-bar
- *= require ./vendors/notify
- *= require ./vendors/select2
- *= require ./vendors/fullcalendar
- *= require_tree ./customer
- */
-
+//= require ./vendors/moment.min
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require ./vendors/angular
+//= require ./vendors/bootstrap
+//= require ./vendors/loading-bar
+//= require ./vendors/notify
+//= require ./vendors/select2
+//= require ./vendors/fullcalendar
+// require_tree ./customer
 
 //$(document).on('ready page:load', function(){
 //    $('#advertisement_category_id, #advertisement_subcategory_id').select2();
