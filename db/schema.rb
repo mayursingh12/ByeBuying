913e482f060ff4ceddabcf760313b06203c9c001
rahulwadhwa <wadhwarahul@gmail.com>
2016-07-14 18:16:09 +0530

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713035548) do
+ActiveRecord::Schema.define(version: 20160714053405) do
 
   create_table "advertisement_images", force: :cascade do |t|
     t.integer  "advertisement_id",   limit: 4
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 20160713035548) do
   add_index "advertisements", ["deleted_at"], name: "index_advertisements_on_deleted_at", using: :btree
 
   create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "states", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
