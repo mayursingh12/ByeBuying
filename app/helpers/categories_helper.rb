589db427a88a60e9bc7aa45fd472d2a5bb285250
Mayur <mayursingh1220@gmail.com>
2016-07-16 00:46:34 +0530

    minor

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -4,8 +4,24 @@ module CategoriesHelper
     Category.all.collect{ |a| [a.name, a.id] }
   end
 
+  def service_category_collection
+    category_ids =  Subcategory.where(subcategory_type: 'Service').map {|x| x.category_id }.uniq
+    Category.where(id: category_ids).collect{ |a| [a.name, a.id] }
+  end
+
   def product_category_collection
-    Category.where(name: ['Health & Wellness', 'Kids', 'Interests & Hobbies', 'Others']).collect{ |a| [a.name, a.id] }
+    category_ids =  Subcategory.where(subcategory_type: 'Product').map {|x| x.category_id }.uniq
+    Category.where(id: category_ids).collect{ |a| [a.name, a.id] }
+  end
+
+  def membership_category_collection
+    category_ids =  Subcategory.where(subcategory_type: 'Membership').map {|x| x.category_id }.uniq
+    Category.where(id: category_ids).collect{ |a| [a.name, a.id] }
+  end
+
+  def space_category_collection
+    category_ids =  Subcategory.where(subcategory_type: 'Space').map {|x| x.category_id }.uniq
+    Category.where(id: category_ids).collect{ |a| [a.name, a.id] }
   end
 
 end
