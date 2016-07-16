d0927f2a51b9b44c9bfc6969e4119dbe2d8ba6e3
Mayur <mayursingh1220@gmail.com>
2016-07-16 19:02:48 +0530

    update

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -20,13 +20,13 @@ module CategoriesHelper
   end
 
   def space_category_collection
-    category_ids =  Subcategory.where(subcategory_type: 'Space').map {|x| x.category_id }.uniq
+    category_ids =  Subcategory.where(subcategory_type: 'Commercial Space').map {|x| x.category_id }.uniq
     Category.where(id: category_ids).collect{ |a| [a.name, a.id] }
   end
 
   def membership_and_space
     membership_category_ids = Subcategory.where(subcategory_type: 'Membership').map {|x| x.category_id }.uniq
-    space_category_ids =  Subcategory.where( subcategory_type: 'Space').map {|x| x.category_id }.uniq
+    space_category_ids =  Subcategory.where( subcategory_type: 'Commercial Space').map {|x| x.category_id }.uniq
 
     category_ids = []
 
