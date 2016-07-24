cd8250c9e1d9857f064b008b09ff83a4cbf36c14
Mayur <mayursingh1220@gmail.com>
2016-07-24 17:42:10 +0530

    tmp links

--- config/routes.rb
+++ config/routes.rb
@@ -107,7 +107,7 @@ Rails.application.routes.draw do
   # ==========Temp URL==============
   # ====== Must be delete ==========
 
-  # get 'barter/landing_page' => 'welcome#index'
-  # get 'barter/about' => 'welcome#about_us'
+  get 'barter/landing_page' => 'welcome#index'
+  get 'barter/about' => 'welcome#about_us'
 
 end
