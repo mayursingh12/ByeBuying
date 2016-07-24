3b9d74bc528193c5f53f8f8c1d15cfaf36d3cb7a
Mayur <mayursingh1220@gmail.com>
2016-07-24 11:54:49 +0530

    add barter and image size

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -6,7 +6,7 @@ class ProductImage < ActiveRecord::Base
 
   DEFAULT_SIZE = :size_500x500
 
-  has_attached_file :image, styles: { size_500x500: '500x500' }
+  has_attached_file :image, styles: { size_500x500: '500x500!' }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
