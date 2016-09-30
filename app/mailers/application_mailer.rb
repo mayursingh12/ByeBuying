15d6b8a9c9e1dbd1197e7e8efd604e265d1f0e34
Mayur <mayursingh1220@gmail.com>
2016-09-30 23:29:58 +0530

    email test

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'info@byebuying.com'
+  # default from: 'info@byebuying.com'
+  default from: 'mayurkhan71@gmail.com'
   layout 'mailer'
 end
