c76a5051f30d688c37fcd7315c2e47f5dd12ba1c
Rahul <rahul.wadhwa@jetsetgo.in>
2016-10-19 16:29:30 +0530

    update

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,30 +1,89 @@
 module CategoriesHelper
 
   def category_for_collection
-    Category.where(is_verified: true).collect{ |a| [a.name, a.id] }
+    categories = []
+    x = []
+    tmp_array = []
+    other_object = Category.where(is_verified: true,name: 'Others' ).last
+    tmp_array = Category.where(is_verified: true).where.not(name: ['Others']).collect{ |a| [a.name, a.id] }
+    if other_object.present?
+      x = [[other_object.name, other_object.id]]
+    end
+    categories = tmp_array + x
+    categories
   end
 
   def service_category_collection
+
+    categories = []
+    x = []
+    tmp_array = []
+
     category_ids =  Subcategory.where(is_verified: true, subcategory_type: 'Service').map {|x| x.category_id }.uniq
-    Category.where(is_verified: true, id: category_ids).collect{ |a| [a.name, a.id] }
+
+    other_object = Category.where(id: category_ids,is_verified: true,name: 'Others' ).last
+    tmp_array = Category.where(id: category_ids, is_verified: true).where.not(name: ['Others']).collect{ |a| [a.name, a.id] }
+    if other_object.present?
+      x = [[other_object.name, other_object.id]]
+    end
+    categories = tmp_array + x
+    categories
   end
 
   def product_category_collection
+    categories = []
+    x = []
+    tmp_array = []
+
     category_ids =  Subcategory.where(is_verified: true, subcategory_type: 'Product').map {|x| x.category_id }.uniq
-    Category.where(is_verified: true, id: category_ids).collect{ |a| [a.name, a.id] }
+
+    other_object = Category.where(id: category_ids, is_verified: true,name: 'Others' ).last
+    tmp_array = Category.where(id: category_ids, is_verified: true).where.not(name: ['Others']).collect{ |a| [a.name, a.id] }
+    if other_object.present?
+      x = [[other_object.name, other_object.id]]
+    end
+    categories = tmp_array + x
+    categories
   end
 
   def membership_category_collection
+    categories = []
+    x = []
+    tmp_array = []
+
     category_ids =  Subcategory.where(is_verified: true, subcategory_type: 'Membership').map {|x| x.category_id }.uniq
-    Category.where(is_verified: true, id: category_ids).collect{ |a| [a.name, a.id] }
+
+    other_object = Category.where(id: category_ids, is_verified: true,name: 'Others' ).last
+    tmp_array = Category.where(id: category_ids, is_verified: true).where.not(name: ['Others']).collect{ |a| [a.name, a.id] }
+    if other_object.present?
+      x = [[other_object.name, other_object.id]]
+    end
+    categories = tmp_array + x
+    categories
   end
 
   def space_category_collection
+
+    categories = []
+    x = []
+    tmp_array = []
+
     category_ids =  Subcategory.where(is_verified: true, subcategory_type: 'Commercial Space').map {|x| x.category_id }.uniq
-    Category.where(is_verified: true, id: category_ids).collect{ |a| [a.name, a.id] }
+
+    other_object = Category.where(id: category_ids, is_verified: true,name: 'Others' ).last
+    tmp_array = Category.where(id: category_ids, is_verified: true).where.not(name: ['Others']).collect{ |a| [a.name, a.id] }
+    if other_object.present?
+      x = [[other_object.name, other_object.id]]
+    end
+    categories = tmp_array + x
+    categories
   end
 
   def service_category_collertion
+    categories = []
+    x = []
+    tmp_array = []
+
     membership_category_ids = Subcategory.where(is_verified: true, subcategory_type: 'Membership').map {|x| x.category_id }.uniq
     space_category_ids =  Subcategory.where(is_verified: true, subcategory_type: 'Commercial Space').map {|x| x.category_id }.uniq
     service_category_ids =  Subcategory.where(is_verified: true, subcategory_type: 'Service').map {|x| x.category_id }.uniq
@@ -35,7 +94,15 @@ module CategoriesHelper
     category_ids << space_category_ids if space_category_ids.present?
     category_ids << service_category_ids if service_category_ids.present?
 
-    Category.where(is_verified: true, id: category_ids).collect{ |a| [a.name, a.id] }
+    category_ids = category_ids.uniq
+
+    other_object = Category.where(id: category_ids, is_verified: true,name: 'Others' ).last
+    tmp_array = Category.where(id: category_ids, is_verified: true).where.not(name: ['Others']).collect{ |a| [a.name, a.id] }
+    if other_object.present?
+      x = [[other_object.name, other_object.id]]
+    end
+    categories = tmp_array + x
+    categories
   end
 
 end
