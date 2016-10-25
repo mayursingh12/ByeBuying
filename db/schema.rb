774a7f401a40ea759a56470d093510dd23d04d44
Rahul <rahul.wadhwa@jetsetgo.in>
2016-10-25 11:27:01 +0530

    blog

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018145459) do
+ActiveRecord::Schema.define(version: 20161025055511) do
 
   create_table "advertisement_images", force: :cascade do |t|
     t.integer  "advertisement_id",   limit: 4
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 20161018145459) do
   end
 
   add_index "advertisements", ["deleted_at"], name: "index_advertisements_on_deleted_at", using: :btree
+
+  create_table "blogs", force: :cascade do |t|
+    t.string   "heading",     limit: 255
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",                        limit: 255
