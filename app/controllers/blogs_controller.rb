430688769ea344f2726615feba51884bafb4046b
Rahul <rahul.wadhwa@jetsetgo.in>
2016-10-29 21:49:18 +0530

    blog show

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,9 +1,23 @@
 class BlogsController < ApplicationController
 
+  before_action :set_categories
+
+  before_action :set_blog, only: [:show]
+
   layout 'default'
 
   def index
     @blogs = Blog.all
+  end
+
+  def show
+
+  end
+
+  private
+
+  def set_blog
+    @blog = Blog.find params[:id]
   end
 
 end
