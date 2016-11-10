8abe490d7a5063ed134f8d6350f12bb0489466b6
Rahul <rahul.wadhwa@jetsetgo.in>
2016-11-10 19:23:00 +0530

    rating

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102064504) do
+ActiveRecord::Schema.define(version: 20161110121731) do
 
   create_table "advertisement_images", force: :cascade do |t|
     t.integer  "advertisement_id",   limit: 4
@@ -178,8 +178,9 @@ ActiveRecord::Schema.define(version: 20161102064504) do
     t.integer  "rateable_id",   limit: 4
     t.string   "rateable_type", limit: 255
     t.integer  "user_id",       limit: 4
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.text     "feedback",      limit: 65535
   end
 
   create_table "searches", force: :cascade do |t|
